f1f8860497b0d7cc8728f75862bb65d5884f5a78
AlannaHazlett <alanna.lohff@gmail.com>
2025-03-12 02:22:41 +0000

    Lab 7: Updated crontab

--- crontab.sh
+++ crontab.sh
@@ -21,10 +21,13 @@
 # For more information see the manual pages of crontab(5) and cron(8)
 # 
 # m h  dom mon dow   comma
-31 14 6-14 3 1-5 cd /home/ubuntu/SP25_DS5111_uwa6xv; source env/bin/activate; make ygainers.csv; python ygainers_crontab_csv.py; make clean_ygainers
-30 17 6-14 3 1-5 cd /home/ubuntu/SP25_DS5111_uwa6xv; source env/bin/activate; make ygainers.csv; python ygainers_crontab_csv.py; make clean_ygainers
-01 21 6-14 3 1-5 cd /home/ubuntu/SP25_DS5111_uwa6xv; source env/bin/activate; make ygainers.csv; python ygainers_crontab_csv.py; make clean_ygainers
-31 14 6-14 3 1-5 cd /home/ubuntu/SP25_DS5111_uwa6xv; source env/bin/activate; make wsjgainers.csv; python wsjgainers_crontab_csv.py; make clean_wsjgainers
-30 17 6-14 3 1-5 cd /home/ubuntu/SP25_DS5111_uwa6xv; source env/bin/activate; make wsjgainers.csv; python wsjgainers_crontab_csv.py; make clean_wsjgainers
-01 21 6-14 3 1-5 cd /home/ubuntu/SP25_DS5111_uwa6xv; source env/bin/activate; make wsjgainers.csv; python wsjgainers_crontab_csv.py; make clean_wsjgainers
-45 22 6-14 3 1-5 cd /home/ubuntu/SP25_DS5111_uwa6xv; source env/bin/activate; echo "this is a test" > /home/ubuntu/SP25_DS5111_uwa6xv/collected_data/testfile.txt
+31 13 * * * cd /home/ubuntu/SP25_DS5111_uwa6xv; . env/bin/activate; make clean_ygainers;  make ygainers.csv; python ygainers_crontab_csv.py; make clean_ygainers; make clean_wsjgainers; make wsjgainers.csv; python wsjgainers_crontab_csv.py; make clean_wsjgainers
+30 16 * * * cd /home/ubuntu/SP25_DS5111_uwa6xv; . env/bin/activate; make clean_ygainers;  make ygainers.csv; python ygainers_crontab_csv.py; make clean_ygainers; make clean_wsjgainers; make wsjgainers.csv; python wsjgainers_crontab_csv.py; make clean_wsjgainers
+01 20 * * * cd /home/ubuntu/SP25_DS5111_uwa6xv; . env/bin/activate; make clean_ygainers;  make ygainers.csv; python ygainers_crontab_csv.py; make clean_ygainers; make clean_wsjgainers; make wsjgainers.csv; python wsjgainers_crontab_csv.py; make clean_wsjgainers
+#31 13 6-14 * * cd /home/ubuntu/SP25_DS5111_uwa6xv; . env/bin/activate; make clean_wsjgainers; make wsjgainers.csv; python wsjgainers_crontab_csv.py; make clean_wsjgainers
+#30 16 6-14 * * cd /home/ubuntu/SP25_DS5111_uwa6xv; . env/bin/activate; make clean_wsjgainers; make wsjgainers.csv; python wsjgainers_crontab_csv.py; make clean_wsjgainers
+#01 20 6-14 * * cd /home/ubuntu/SP25_DS5111_uwa6xv; . env/bin/activate; make clean_wsjgainers; make wsjgainers.csv; python wsjgainers_crontab_csv.py; make clean_wsjgainers
+#52 16 6-14 * * cd home/ubuntu/SP25_DS5111_uwa6xv; . env/bin/activate; make clean_wsjgainers;  make wsjgainers.csv; python wsjgainers_crontab_csv.py; make clean_wsjgainers
+#51 13 6-14 3 1-5 cd /home/ubuntu/SP25_DS5111_uwa6xv; . env/bin/activate; echo "this is a test" > /home/ubuntu/SP25_DS5111_uwa6xv/collected_data/testfile.txt
+#* * * * * cd /home/ubuntu/SP25_DS5111_uwa6xv; . env/bin/activate; make clean_ygainers;  make ygainers.csv; python ygainers_crontab_csv.py; make clean_ygainers; make clean_wsjgainers; make wsjgainers.csv; python wsjgainers_crontab_csv.py; make clean_wsjgainers 
+#* * * * * cd /home/ubuntu/SP25_DS5111_uwa6xv; . env/bin/activate; make clean_wsjgainers; make wsjgainers.csv; date >> debug.txt; python wsjgainers_crontab_csv.py; 
