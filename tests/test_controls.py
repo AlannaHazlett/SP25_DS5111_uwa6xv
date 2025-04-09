361ddbc6ab336cb471db48c7aa746dea85711134
AlannaHazlett <alanna.lohff@gmail.com>
2025-04-09 23:47:29 +0000

    Updated test_controls.py to include python version 3.13

--- tests/test_controls.py
+++ tests/test_controls.py
@@ -12,7 +12,7 @@ print(type(sys.version))
 def test_python_version():
     """Test that the Python version is 3.10 or 3.11 or 3.12"""
 
-    assert sys.version[:4] in ["3.10", "3.11", "3.12"], "Python version is lower than 3.10"
+    assert sys.version[:4] in ["3.10", "3.11", "3.12", "3.13"], "Python version is lower than 3.10"
 
 def test_os_system():
     """Test that the OS system name is correctly identified"""
